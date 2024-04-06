733e6a2079e328e6c5239f9dc13a6c7d9889eb64
Bogdan Feher <bogdan.feher@gmail.com>
2024-04-06 03:55:47 -0400

    incomplete js navbar implementation

---
+++ index.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+}
